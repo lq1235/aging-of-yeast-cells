2ff5f8aa1f79eb54abfd7546910486d5cae8303c
linqi wang <138594192+lq1235@users.noreply.github.com>
2025-10-18 17:21:06 +0800

    Add files via upload

---
+++ Gene oscillator-as0-Time evolution series/FourEquation.m
@@ -0,0 +1,80 @@
+clc
+clear
+fileID = fopen('output.txt', 'w'); 
+fprintf(fileID, 'a1 \n'); 
+a1 = 0.1:0.1:1; 
+% a1 = 0:0.001:1.094; 
+% a1 = 1.1:0.1:5.0; 
+b = length(a1);
+
+for i = 1:b
+    i
+    as01 = a1(i); 
+
+    [t, x] = ode45(@(t, x) fun(t, x, as01), [0, 20500], [10.9630341446514 112.165963264298 252.822731958915 135.751824484358]);
+
+    mS_Value = x(end, 1);
+    mH_Value = x(end, 2);
+    Sir2_Value = x(end, 3);
+    HAP_Value = x(end, 4);
+    
+   
+    fprintf(fileID, '%.8f\t%.8f\t%.8f\t%.8f\t%.8f\n', as01, mS_Value, mH_Value, Sir2_Value, HAP_Value);
+    %fprintf('a1:%.5f,Max: %.2f, Min: %.2f\n', ah01,max(x(10000:end, 3)), min(x(10000:end, 3)));
+end
+fclose(fileID);
+
+
+figure(1)
+
+plot(t, x(:, 1)); % mS
+hold on
+plot(t, x(:, 2)); % mH
+legend('mS', 'mH')
+xlabel('t')
+ylabel('x')
+
+figure(2)
+plot(t, x(:, 3)); % S
+hold on
+plot(t, x(:, 4)); % H
+legend('S', 'H')
+xlabel('t')
+ylabel('x')
+
+
+figure(3)
+plot(x(:, 4), x(:, 3));
+xlim([0 1000]);
+ylim([0 1200]);
+xlabel('H')
+ylabel('S')
+
+
+function dx = fun(t, x, as01)
+  
+    as = 30.5;
+    ah = 183;
+    ah0 = 0.1; 
+    as0 = as01;
+    beta = 3.7; % 4.6
+    dm = 0.3;
+    dh = 3.8;
+    ds = 0.2;
+    Kh = 326;
+    Ks = 185;
+    n1 = 3;
+    n2 = 4.8;
+
+    mS = x(1);
+    mH = x(2);
+    S = x(3);
+    H = x(4);
+
+    dx1dt = as0 + as * H^n1 / (Kh^n1 + H^n1) - dm * mS;
+    dx2dt = ah0 + ah * Ks^n2 / (Ks^n2 + S^n2) - dm * mH;
+    dx3dt = beta * mS - ds * S;
+    dx4dt = beta * mH - dh * H;
+
+    dx = [dx1dt; dx2dt; dx3dt; dx4dt];
+end
